a358197fb73094a9bc1346145af794cd319b361b
Julia Georgiew <java2011@me.com>
2023-07-20 14:05:21 +0200

    ignore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,10 @@
 export const environment = {
- 
+  firebase: {
+    projectId: 'jg-myfirstlogin',
+    appId: '1:928960705548:web:3b2662f0054847f5b86a29',
+    storageBucket: 'jg-myfirstlogin.appspot.com',
+    apiKey: 'AIzaSyCZ_ZvW6zHkv0FkyfTiMsWZ-XZUkecTe6c',
+    authDomain: 'jg-myfirstlogin.firebaseapp.com',
+    messagingSenderId: '928960705548',
+  },
 };
